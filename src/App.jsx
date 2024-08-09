d6904950f44c3ad48b82b4895a17a951bb71fc9d
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-08-09 23:14:00 +0530

    App.jsx run

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import axios from '/axios'
 import './App.css'
 
 function App() {
   const [jokes, setJokes] = useState([])
 
+  useEffect(() => {
+    axios.get('/api/jokes')
+    .then((response) => {
+      setJokes(response.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  })
+
   return (
     <>
      <h1>Sonam Singh Rajput</h1>
      <p>JOKES: {jokes.length}</p>
+     {
+      jokes.map((joke, index) => (
+        <div key={joke.id}>
+        <h3>{joke.title}</h3>
+        <p>{joke.content}</p>
+        </div>
+      ))
+     }
     </>
   )
 }
